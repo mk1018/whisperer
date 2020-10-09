cb39724833e6d8ce4c9760017a11d2d50928962a
mk1018 <ktmn1018@yahoo.co.jp>
2020-10-09 20:12:55 +0900

    完了

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -3,28 +3,63 @@
 <template>
   <li class="item">
     <div class="user-box">
+      <div class="avatar" :style="'background-image: url('+user.photoURL+')'"></div>
+      <p class="user-name">{{user.name}}</p>
+    </div>
 
-      <div class="avatar" :style="'background-image: url('+user.photoURL+')'">
-      </div> <!-- ここを変更 -->
+    <div v-if="editing" class="editor">
+      <textarea v-model="whisper.content" placeholder="edit whisper" @keypress.enter="updateWhisper"></textarea>
+      <p class="message">Press Enter to Whisper</p>
+    </div>
+    <div v-else class="content" v-html="whisper.content"></div>
 
-        <p class="user-name">{{user.name}}</p> <!-- ここを変更 -->
+    <button v-if="currentUser && currentUser.uid == user.id" @click="showBtns = !showBtns">
+      <fa icon="ellipsis-v" />
+    </button>
 
-      </div>
-    <div class="content" v-html="whisper.content">
+    <div v-if="showBtns" class="controls">
+      <li @click="editing = !editing">edit</li>
+      <li @click="deleteWhisper" style="color: red">delete</li>
     </div>
   </li>
 </template>
 
 <script>
 import { db } from '../main'
+import { auth } from '../main'
 
 export default {
   props: ['id','uid'],
   data () {
     return {
       whisper: {},
-      user: {}
+      user: {},
+      currentUser: {},
+      showBtns: false,
+      editing: false,
     }
+  },
+  methods: {
+    deleteWhisper () {
+      if (window.confirm('Are You Sure to Delete This Whisper?')) {
+        db.collection('whispers').doc(this.$props.id).delete()
+      }
+    },
+    updateWhisper () {
+      const date = new Date()
+      db.collection('whispers').doc(this.whisper.id).set({
+        'content': this.whisper.content,
+        'date': date
+      }, { merge: true })
+      .then(
+        this.editing = false
+      )
+    }
+  },
+  created () {
+    auth.onAuthStateChanged(user => {
+      this.currentUser = user
+    })
   },
   firestore () {
     return {
@@ -44,6 +79,30 @@ export default {
   flex-wrap no-wrap
   justify-content flex-start
   position relative
+  .editor
+    position relative
+    width 100%
+    textarea
+      background transparent
+      resize none
+      height 80px
+      width 96%
+      border none
+      padding 10px 2%
+      font-size .9rem
+      font-weight lighter
+      &:focus
+        outline none
+    .message
+      opacity 0
+      position absolute
+      bottom 5px
+      right 10px
+      transition .2s
+      font-size .8rem
+    &:hover
+      .message
+        opacity 1
   &:first-child
     border none
   &:hover
@@ -64,4 +123,36 @@ export default {
       width 50px
   .content
     padding 10px
+button
+    position absolute
+    top 5px
+    right 0
+    background transparent
+    color #555
+    font-size .9rem
+    opacity 0
+    transition .2s
+  .controls
+    background white
+    position absolute
+    top 5px
+    right 35px
+    box-shadow 0 0 5px rgba(0,0,0,.05)
+    border-radius 3px
+    opacity 0
+    li
+      padding 5px 20px
+      border-top 1px solid #eee
+      cursor pointer
+      &:first-child
+        border none
+  &:first-child
+    border none
+  &:hover
+    background rgba(0,0,0,.02)
+    .content
+    button
+      opacity 1
+    .controls
+      opacity 1
 </style>
--- src/views/User.vue
+++ src/views/User.vue
@@ -4,25 +4,55 @@
       <div class="avatar" :style="'background-image: url('+user.photoURL+')'"></div>
       <div class="texts">
         <h1>{{user.name}}</h1>
+				<p v-if="myWhispers.length > 1">{{myWhispers.length}} whispers</p>
+        <p v-else>{{myWhispers.length}} whisper</p>
       </div>
+    </div>
+
+		<div class="list">
+			<Editor :currentUser="currentUser"/>
+      <Item
+        v-for="whisper in orderBy(myWhispers,'date',-1)"
+        :key="whisper.id"
+        :id="whisper.id"
+        :uid="whisper.uid"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import { db } from '../main'
+import { auth } from '../main'
+import firebase from 'firebase'
+import Item from '@/components/Item'
+import Vue2Filters from 'vue2-filters'
+import Editor from '@/components/Editor'
 
 export default {
+	components: {
+		Item,
+		Editor
+  },
   data () {
     return {
-      user: {}
+      user: {},
+			myWhispers: [],
+			currentUser: {}
     }
   },
   firestore () {
     return {
-      user: db.collection('users').doc(this.$route.params.uid)
+			user: db.collection('users').doc(this.$route.params.uid),
+			myWhispers: db.collection('whispers').where('uid','==',this.$route.params.uid)
     }
-  }
+	},
+	created () {
+    auth.onAuthStateChanged(user => {
+      this.currentUser = user
+    })
+  },
+	mixins: [Vue2Filters.mixin]
 }
 </script>
 
